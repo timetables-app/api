a1003e21b8cd4c102956254721227831934f27b5
pwalus <patrykwal12@gmail.com>
2019-04-09 19:20:21 +0200

    Controller for companies

--- src/main/java/app.timetables.api/community/controllers/CompanyController.java
+++ src/main/java/app.timetables.api/community/controllers/CompanyController.java
@@ -1,21 +1,36 @@
-package app.timetables.api.community.controller;
+package app.timetables.api.community.controllers;
 
 import app.timetables.api.community.domain.Company;
 import app.timetables.api.community.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@Controller
 @RestController
+@RequestMapping("/companies")
 public class CompanyController {
 
     @Autowired
     private CompanyRepository companyRepository;
 
-    @PostMapping("/approve/company/{id}")
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<Company> getCompany(@PathVariable Long id) {
+        return ResponseEntity.of(companyRepository.findById(id));
+    }
+
+    @GetMapping(produces = "application/json")
+    public Iterable<Company> getCompanies(@RequestParam(defaultValue = "10") Integer size, @RequestParam(defaultValue = "0") Integer page) {
+        Pageable pageable = PageRequest.of(page, size);
+        return companyRepository.findAll(pageable);
+    }
+
+    @PostMapping(value = "/approve/company/{id}", produces = "application/json")
     public ResponseEntity<Company> approveCompany(@PathVariable Integer id) {
         Optional<Company> optionalCompany = companyRepository.findById(Long.valueOf(id));
         if (optionalCompany.isPresent()) {
@@ -26,7 +41,7 @@ public class CompanyController {
         return ResponseEntity.of(optionalCompany);
     }
 
-    @PostMapping("/block/company/{id}")
+    @PostMapping(value = "/block/company/{id}", produces = "application/json")
     public ResponseEntity<Company> blockCompany(@PathVariable Integer id) {
         Optional<Company> optionalCompany = companyRepository.findById(Long.valueOf(id));
         if (optionalCompany.isPresent()) {
--- src/main/java/app.timetables.api/community/repository/CompanyRepository.java
+++ src/main/java/app.timetables.api/community/repository/CompanyRepository.java
@@ -2,8 +2,8 @@ package app.timetables.api.community.repository;
 
 import app.timetables.api.community.domain.Company;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
+@Repository
 public interface CompanyRepository extends PagingAndSortingRepository<Company, Long> {
 }
